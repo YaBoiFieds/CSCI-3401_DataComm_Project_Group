ab31993b44e88d6ce997564c19e7e18332bda8c8
anastasiaaltamirano <46826948+anastasiaaltamirano@users.noreply.github.com>
2021-05-07 09:47:34 -1000

    working unittest of e_emnt and v_amnt methods

---
+++ SurvivableLeastCostPath_e_amnt_v_amnt_test.py
@@ -0,0 +1,17 @@
+import unittest
+from SurvivableLeastCostPath import *
+
+class EAmntVAmntTestCase(unittest.TestCase):
+    def test_v_amnt_accurate(self):
+        # set up
+        testGraph = MakeGraph({'a', 'b', 'c'}, {('a', 'b'), ('a', 'c')})
+
+        # assert statements
+        self.assertEqual(testGraph.e_amnt(), 2)
+        self.assertEqual(testGraph.v_amnt(), 3)
+
+        print("Amount of vertices and edges accurate.")
+
+
+if __name__ == '__main__':
+    unittest.main()
