444bbe805374ba8951a6741ce2aadbc5e5b1d0fa
anastasiaaltamirano <46826948+anastasiaaltamirano@users.noreply.github.com>
2021-05-07 09:48:41 -1000

    working unittest of degree method

---
+++ SurvivableLeastCostPath_degree_test.py
@@ -0,0 +1,18 @@
+import unittest
+from SurvivableLeastCostPath import *
+
+class DegreeTestCase(unittest.TestCase):
+    def test_degree_accurate(self):
+
+        # set up
+        testGraph = MakeGraph({'a', 'b', 'c'}, {('a', 'b'), ('a', 'c')})
+
+        # assert statements
+        self.assertEqual(testGraph.degree('a'), 2, 'Broken somewhere.')
+        self.assertEqual(testGraph.degree('b'), 1, 'Broken somewhere')
+        self.assertEqual(testGraph.degree('c'), 1, 'Broken somewhere.')
+
+        print("Degree method accurate.")
+
+if __name__ == '__main__':
+    unittest.main()
