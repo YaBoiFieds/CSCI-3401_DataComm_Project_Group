4af32210061621b2f19e91fbfb7f49ccc1882ef8
anastasiaaltamirano <46826948+anastasiaaltamirano@users.noreply.github.com>
2021-05-07 09:46:59 -1000

    working unittest of addEdge method

---
+++ SurvivableLeastCostPath_addEdge_test.py
@@ -0,0 +1,19 @@
+import unittest
+from SurvivableLeastCostPath import *
+
+class AddEdgeTestCase(unittest.TestCase):
+    def test_addEdge_works(self):
+        # set up
+        testGraph = MakeGraph({'a', 'b', 'c'}, {('a', 'b'), ('a', 'c')})
+        testGraph.addEdge('d', 'b')
+
+        # assert statements
+        self.assertEqual(testGraph.degree('a'), 2, 'Broken somewhere.')
+        self.assertEqual(testGraph.degree('b'), 2, 'Broken somewhere')
+        self.assertEqual(testGraph.degree('c'), 1, 'Broken somewhere.')
+        self.assertEqual(testGraph.degree('d'), 1, 'Broken somewhere.')
+
+        print("Edge added correctly.")
+
+if __name__ == '__main__':
+    unittest.main()
