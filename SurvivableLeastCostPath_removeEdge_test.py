71b24ba7ce018144a7e84668709f017cc4b83b13
anastasiaaltamirano <46826948+anastasiaaltamirano@users.noreply.github.com>
2021-05-07 09:48:20 -1000

    working unittest of removeEdge method

---
+++ SurvivableLeastCostPath_removeEdge_test.py
@@ -0,0 +1,17 @@
+import unittest
+from SurvivableLeastCostPath import *
+
+class RemoveEdgeTest(unittest.TestCase):
+    def test_removeEdge_accurate(self):
+        # set up
+        testGraph = MakeGraph({'a', 'b', 'c'}, {('a', 'b'), ('a', 'c')})
+        testGraph.removeEdge('a', 'b')
+
+        # assert statements
+        self.assertEqual(testGraph.e_amnt(), 1)
+
+        print("Edge removed, amount of vertices and edges accurate.")
+
+
+if __name__ == '__main__':
+    unittest.main()
